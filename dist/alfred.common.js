15796a6e69d5c42b7224c302787b4be73cb6c673
Hubert Prein <gprein@gmail.com>
2025-02-17 15:45:31 +0100

    Rebuild library

--- dist/alfred.common.js
+++ dist/alfred.common.js
@@ -9882,7 +9882,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-;// ./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=template&id=29aa4630
+;// ./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=template&id=bdea2a74
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
@@ -10090,7 +10090,7 @@ var staticRenderFns = [function () {
   })]);
 }];
 
-;// ./resources/js/components/Alfred.vue?vue&type=template&id=29aa4630
+;// ./resources/js/components/Alfred.vue?vue&type=template&id=bdea2a74
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.push.js
 var es_array_push = __webpack_require__(4114);
@@ -15041,15 +15041,20 @@ var sweetalert2_all_default = /*#__PURE__*/__webpack_require__.n(sweetalert2_all
      * @param {Object} item
      */
     handleItemAutocomplete(item) {
+      // Make sure we have the correct item
+      if ('obj' in item) {
+        item = item.obj;
+      }
+
       // Autocomplete to prefix, when the current phrase has the characters within the prefix
       if ((item.prefix || null) && item.prefix.includes(this.alfred.phrase)) {
         this.alfred.phrase = item.prefix;
         return;
       }
       let newPhrase = [];
-      for (let itemNamePart in item.name.toLowerCase().replace(/[-_:]/g, '').split(' ')) {
-        for (let alfredPhrasePart in this.alfred.phrase.split(' ')) {
-          if (itemNamePart.includes(alfredPhrasePart)) {
+      for (let itemNamePart of item.name.toLowerCase().replace(/[-_:]/g, '').split(' ')) {
+        for (let alfredPhrasePart of this.alfred.phrase.split(' ')) {
+          if (itemNamePart.includes(alfredPhrasePart) && !newPhrase.includes(itemNamePart)) {
             newPhrase.push(itemNamePart);
           }
         }
@@ -15239,10 +15244,10 @@ var sweetalert2_all_default = /*#__PURE__*/__webpack_require__.n(sweetalert2_all
 });
 ;// ./resources/js/components/Alfred.vue?vue&type=script&lang=js
  /* harmony default export */ var components_Alfredvue_type_script_lang_js = (Alfredvue_type_script_lang_js); 
-;// ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-12.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-12.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-12.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=style&index=0&id=29aa4630&prod&lang=css
+;// ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-12.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-12.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-12.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=style&index=0&id=bdea2a74&prod&lang=css
 // extracted by mini-css-extract-plugin
 
-;// ./resources/js/components/Alfred.vue?vue&type=style&index=0&id=29aa4630&prod&lang=css
+;// ./resources/js/components/Alfred.vue?vue&type=style&index=0&id=bdea2a74&prod&lang=css
 
 ;// ./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js
 /* globals __VUE_SSR_CONTEXT__ */
