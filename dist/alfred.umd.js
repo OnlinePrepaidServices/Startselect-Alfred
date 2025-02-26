f6a6b5189c3a69e43d307f2749acc348436babb4
Hubert Prein <gprein@gmail.com>
2025-02-26 16:04:42 +0100

    Rebuild library

--- dist/alfred.umd.js
+++ dist/alfred.umd.js
@@ -9892,7 +9892,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-;// ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=template&id=3cb46e26
+;// ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=template&id=970994b4
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
@@ -10122,11 +10122,11 @@ var staticRenderFns = [function () {
   })]), _c('span', [_c('i', {
     staticClass: "fas fa-arrow-down"
   })]), _c('span', [_vm._v("to navigate")]), _c('span', [_c('i', {
-    staticClass: "fas fa-arrow-turn-down fa-rotate-90"
+    staticClass: "fas fa-directions fa-rotate-180"
   })]), _c('span', [_vm._v("to select")])]);
 }];
 
-;// ./resources/js/components/Alfred.vue?vue&type=template&id=3cb46e26
+;// ./resources/js/components/Alfred.vue?vue&type=template&id=970994b4
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.push.js
 var es_array_push = __webpack_require__(4114);
@@ -13686,6 +13686,7 @@ var sweetalert2_all_default = /*#__PURE__*/__webpack_require__.n(sweetalert2_all
   MAX_POPULAR_ITEMS_ON_INIT: 'maxPopularItemsOnInit',
   TIMEOUT_ACTION: 'timeouts.action',
   TIMEOUT_MESSAGES: 'timeouts.messages',
+  TITLE_ITEMS_EMPTY: 'defaultValues.titleItemsEmpty',
   TITLE_ITEMS_FALLBACK: 'defaultValues.titleItemsFallback',
   TITLE_ITEMS_POPULAR: 'defaultValues.titleItemsPopular',
   TITLE_ITEMS_RESULTS: 'defaultValues.titleItemsResults',
@@ -14683,6 +14684,12 @@ var sweetalert2_all_default = /*#__PURE__*/__webpack_require__.n(sweetalert2_all
         return;
       }
 
+      // No filtered items, filled out phrase and not the registered set of items? Display an empty results title
+      if (this.items.saved.length && !filtered.length && this.getPhrase()) {
+        this.items.title = this.getSetting(settings.TITLE_ITEMS_EMPTY, 'No results');
+        return;
+      }
+
       // Render fuzzy filtered items
       this.renderItems(filtered, false, []);
       this.items.title = this.getSetting(settings.TITLE_ITEMS_RESULTS, 'Results');
@@ -15320,10 +15327,10 @@ var sweetalert2_all_default = /*#__PURE__*/__webpack_require__.n(sweetalert2_all
 });
 ;// ./resources/js/components/Alfred.vue?vue&type=script&lang=js
  /* harmony default export */ var components_Alfredvue_type_script_lang_js = (Alfredvue_type_script_lang_js); 
-;// ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-54.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-54.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-54.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=style&index=0&id=3cb46e26&prod&lang=css
+;// ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-54.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-54.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-54.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./resources/js/components/Alfred.vue?vue&type=style&index=0&id=970994b4&prod&lang=css
 // extracted by mini-css-extract-plugin
 
-;// ./resources/js/components/Alfred.vue?vue&type=style&index=0&id=3cb46e26&prod&lang=css
+;// ./resources/js/components/Alfred.vue?vue&type=style&index=0&id=970994b4&prod&lang=css
 
 ;// ./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js
 /* globals __VUE_SSR_CONTEXT__ */
