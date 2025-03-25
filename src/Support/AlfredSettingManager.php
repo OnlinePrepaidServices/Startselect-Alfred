b9f521873fe624bd5e00a0cdb0cac26908c39317
Hubert Prein <gprein@gmail.com>
2025-03-25 14:13:31 +0100

    Use contract in the setting manager instead

--- src/Support/AlfredSettingManager.php
+++ src/Support/AlfredSettingManager.php
@@ -4,13 +4,14 @@ namespace Startselect\Alfred\Support;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
+use Startselect\Alfred\Contracts\PreferenceManager;
 
 class AlfredSettingManager
 {
     public static function init(): array
     {
-        /** @var DefaultPreferenceManager $preferenceManager */
-        $preferenceManager = App::make(DefaultPreferenceManager::class);
+        /** @var PreferenceManager $preferenceManager */
+        $preferenceManager = App::make(PreferenceManager::class);
 
         return array_merge(Config::get('alfred.settings'), $preferenceManager->settings()->data);
     }
