49088e0a85835500243b1e7d3015c643ca2d49d6
Hubert Prein <gprein@gmail.com>
2025-03-26 11:39:58 +0100

    Remove unnecessary methods from AlfredPreference model

--- src/Models/AlfredPreference.php
+++ src/Models/AlfredPreference.php
@@ -19,41 +19,4 @@ class AlfredPreference extends Model
         'type' => AlfredPreferenceType::class,
         'data' => 'array',
     ];
-
-    public function setData(mixed $key, mixed $value = null, bool $replace = true): static
-    {
-        // Upon replacing, we simply overwrite the current key's value, with the new value
-        // Without replacing, we add the new value to the current value, which becomes an array of values
-        if ($replace) {
-            $items = is_array($key) ? $key : [$key => $value];
-            $data = $this->data;
-
-            foreach ($items as $key => $value) {
-                $data[$key] = $value;
-            }
-
-            $this->data = $data;
-
-            return $this;
-        }
-
-        if (!is_array($key)) {
-            $key = [$key => $value];
-        }
-
-        $this->data = array_merge_recursive($this->data ?? [], $key);
-
-        return $this;
-    }
-
-    public function unsetData(mixed $key): static
-    {
-        $data = $this->data ?? [];
-
-        unset($data[$key]);
-
-        $this->data = $data;
-
-        return $this;
-    }
 }
