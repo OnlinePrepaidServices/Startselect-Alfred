df3f232e4a0eabb7c0828223d85e7a42df6137ee
Hubert Prein <gprein@gmail.com>
2025-04-11 15:16:21 +0200

    Move local storage data syncing to a trait

---
+++ src/Concerns/PreferenceManagerDataSync.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Startselect\Alfred\Concerns;
+
+use Illuminate\Http\Request;
+use Startselect\Alfred\Contracts\PreferenceManager;
+use Startselect\Alfred\Support\PreferenceManagers\LocalStoragePreferenceManager;
+
+trait PreferenceManagerDataSync
+{
+    /**
+     * Update preferences based on the request.
+     */
+    public function syncDataByRequest(PreferenceManager $preferenceManager, Request $request): void
+    {
+        // Only update in case of local storage
+        if (!$preferenceManager instanceof LocalStoragePreferenceManager) {
+            return;
+        }
+
+        // Do we have the request information available?
+        if (!$request->has('storage')) {
+            return;
+        }
+
+        // Update preferences
+        if ($request->has('storage.settings')) {
+            $preferenceManager->settings()->data = $request->input('storage.settings');
+        }
+        if ($request->has('storage.snippets')) {
+            $preferenceManager->snippets()->data = $request->input('storage.snippets');
+        }
+    }
+
+    public function getDataForResponse(PreferenceManager $preferenceManager): array
+    {
+        // Only return preferences in case of local storage
+        if (!$preferenceManager instanceof LocalStoragePreferenceManager) {
+            return [];
+        }
+
+        return [
+            'settings' => $preferenceManager->settings()->data,
+            'snippets' => $preferenceManager->snippets()->data,
+        ];
+    }
+}
--- src/Http/Controllers/AlfredInitiationController.php
+++ src/Http/Controllers/AlfredInitiationController.php
@@ -6,23 +6,18 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Startselect\Alfred\Alfred;
+use Startselect\Alfred\Concerns\PreferenceManagerDataSync;
 use Startselect\Alfred\Contracts\PreferenceManager;
-use Startselect\Alfred\Support\PreferenceManagers\LocalStoragePreferenceManager;
 use Startselect\Alfred\ValueObjects\PageData;
 
 class AlfredInitiationController extends Controller
 {
+    use PreferenceManagerDataSync;
+
     public function __invoke(Request $request, Alfred $alfred, PreferenceManager $preferenceManager): JsonResponse
     {
-        // Update local storage preferences
-        if ($preferenceManager instanceof LocalStoragePreferenceManager && $request->has('storage')) {
-            if ($request->has('storage.settings')) {
-                $preferenceManager->settings()->data = $request->input('storage.settings');
-            }
-            if ($request->has('storage.snippets')) {
-                $preferenceManager->snippets()->data = $request->input('storage.snippets');
-            }
-        }
+        // Sync data with the preference manager
+        $this->syncDataByRequest($preferenceManager, $request);
 
         return new JsonResponse([
             'result' => $alfred->getRegisteredWorkflowSteps(
@@ -30,12 +25,7 @@ class AlfredInitiationController extends Controller
             )->toArray(),
             'settings' => $preferenceManager->settings()->data,
             'snippets' => $preferenceManager->snippets()->data,
-            'storage' => $preferenceManager instanceof LocalStoragePreferenceManager
-                ? [
-                    'settings' => $preferenceManager->settings()->data,
-                    'snippets' => $preferenceManager->snippets()->data,
-                ]
-                : [],
+            'storage' => $this->getDataForResponse($preferenceManager),
         ]);
     }
 }
