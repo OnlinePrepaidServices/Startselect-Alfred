790e3e9fd7ea61b2e7238053275c4b48abe6f5c4
Hubert Prein <gprein@gmail.com>
2025-04-14 11:36:29 +0200

    Add route for saving item settings

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Startselect\Alfred\Http\Controllers\AlfredInitiationController;
+use Startselect\Alfred\Http\Controllers\AlfredItemSettingsController;
 use Startselect\Alfred\Http\Controllers\AlfredWorkflowStepHandlerController;
 
 Route::group(['middleware' => ['web', 'auth']], function () {
     Route::post('alfred/initiate', AlfredInitiationController::class)->name('alfred.initiate');
     Route::post('alfred/handle-workflow-step', AlfredWorkflowStepHandlerController::class)->name('alfred.handle_workflow_step');
+    Route::post('alfred/save-item-settings', AlfredItemSettingsController::class)->name('alfred.save_item_settings');
 });
